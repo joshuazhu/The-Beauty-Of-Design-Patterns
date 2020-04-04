9bce991243f1f9227312a2e9356af6e66747c41b
Josh Zhu <josh.zhu@acloud.guru>
2020-04-04 21:58:04 +1100

    add typescript project

---
+++ dist/app.js
@@ -0,0 +1,6 @@
+const greeting = (message) => {
+    console.log(message);
+};
+const message = 'Hello World';
+greeting(message);
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,7 @@
+const greeting = (message: string) => {
+  console.log(message);
+};
+
+const message = 'Hello World';
+
+greeting(message);
